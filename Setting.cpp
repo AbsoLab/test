9f3c480498de0b0f263e1903ef137b163c2aba77
Hertz <kljj0650@naver.com>
2018-11-24 23:24:02 +0900

    색깔 조정기능 임시 구현

--- Setting.cpp
+++ Setting.cpp
@@ -2,19 +2,20 @@
 #include "Setting.h"
 #include "Shape.h"
 
-Setting::Setting()
-{
+Setting::Setting() {
+
 	shape = new Shape*[50];
 
 	count = 0;
 
 	selected_type = 1;
-	selected_color = 0;
+	selected_color = RGB(0, 0, 0);
 	selected_thickness = 1;
+
 }
 
-void Setting::AddShape(CPoint pt1, CPoint pt2)
-{
-	shape[count++] = new Shape(pt1, pt2, selected_type, selected_thickness, selected_color, selected_color);
+void Setting::AddShape(CPoint pt1, CPoint pt2) {
+
+	shape[count++] = new Shape(pt1, pt2, selected_type, selected_thickness, selected_color, -1);
 }
 
